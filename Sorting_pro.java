33a210ef41c10e06d70603a532771b57298c3a06
Nivethini29 <34852039+Nivethini29@users.noreply.github.com>
2018-04-20 08:58:45 -0700

    Update Sorting_pro.java

--- Sorting_pro.java
+++ Sorting_pro.java
@@ -5,13 +5,17 @@ public class Sorting_pro {
     public static void main(String[] args) {
             
         int[] data=new int[]{3423,4,3,42,3,6,4,8,3,2,3,2,34,2,348,8,234,2,2,3,54,7,56,85,35,4,5};
+        //sorting 
         Arrays.sort(data);
+        
         System.out.println("Asc order");
+        //display
         for(int i=0;i<data.length;i++){
         System.out.print(" "+data[i]+",");
         }
         
         System.out.println("\nDesc order");
+        //display
         for(int i=data.length-1;i>=0;i--){
         System.out.print(" "+data[i]+",");
         }
